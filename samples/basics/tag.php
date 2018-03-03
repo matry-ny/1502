d1cdae6060d9408a14d5498906900db9ca750073
Dmytro Kotenko <d.kotenko@i.ua>
2018-03-03 11:02:33 +0200

    Added tags examples

---
+++ samples/basics/tag.php
@@ -0,0 +1,19 @@
+<?php
+
+// echo 123;
+# echo 123;
+/* echo 123; */
+/*
+ * echo 123;
+ * echo 321;
+ */
+/**
+ * @var array $test
+ */
+# var_dump($test);
+
+echo 'Hello World<br>';
+
+?>
+
+<h1><?= 'Hello 15-02' ?></h1>
